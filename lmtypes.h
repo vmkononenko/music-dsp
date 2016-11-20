3a635393c3deda86f59372071000854f55d0afe5
Volodymyr Kononenko <vmkononenko@gmail.com>
2016-11-20 23:47:35 +0100

    Lmtypes / Add sharp notes to notes' enumeration

--- lmtypes.h
+++ lmtypes.h
@@ -16,11 +16,18 @@ typedef uint8_t octave_t;
 
 typedef enum {
     note_C,
+    note_C_sharp,
     note_D,
+    note_D_sharp,
     note_E,
+    note_E_sharp,
     note_F,
+    note_F_sharp,
     note_G,
+    note_G_sharp,
     note_A,
+    note_A_sharp,
     note_B,
+    note_B_sharp,
     note_Unknown
 } note_t;
