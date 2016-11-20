1653367cb77d7435b50b9f6613710fba75a7fbdc
Volodymyr Kononenko <vmkononenko@gmail.com>
2016-11-20 23:50:19 +0100

    Helpers / Add stdRound()

--- lmhelpers.h
+++ lmhelpers.h
@@ -8,6 +8,7 @@
 
 #include <stdint.h>
 #include <vector>
+
 #include "lmtypes.h"
 
 #ifndef TEST_FRIENDS
@@ -19,6 +20,26 @@ class Helpers {
 TEST_FRIENDS;
 
 public:
-	static uint32_t nextPowerOf2(uint32_t);
-	static std::vector<complex_t> timeDomain2ComplexVector(amplitude_t *, uint32_t);
+    static uint32_t nextPowerOf2(uint32_t);
+    static std::vector<complex_t> timeDomain2ComplexVector(amplitude_t *, uint32_t);
+
+    /**
+     * Round number with requested precision
+     *
+     * @param   n       number to round up
+     * @param   scale   numeric scale - number of digits after decimal point
+     * @return  rounded number with scale
+     */
+    template<typename T> static T stdRound(T n, uint8_t scale)
+    {
+        if (scale == 0) {
+            n = round(n);
+        } else {
+            n *= (10 * scale);
+            n = round(n);
+            n /= scale;
+        }
+
+        return n;
+    }
 };
