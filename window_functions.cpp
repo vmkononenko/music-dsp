fe3e65f927479156246f1f5272de3bbcb888393d
Volodymyr Kononenko <vmkononenko@gmail.com>
2017-04-20 21:57:14 +0200

    Fix denominator in window functions

--- window_functions.cpp
+++ window_functions.cpp
@@ -12,15 +12,15 @@
 void WindowFunctions::applyHamming(amplitude_t *points, uint32_t pointsCnt)
 {
     for (uint32_t i = 0; i < pointsCnt; i++) {
-        points[i] = points[i] * (0.54 - 0.46 * cos(2 * M_PI * i / pointsCnt));
+        points[i] = points[i] * (0.54 - 0.46 * cos(2 * M_PI * i / (pointsCnt - 1)));
     }
 }
 
 void WindowFunctions::applyBlackman(amplitude_t *points, uint32_t pointsCnt)
 {
     for (uint32_t i = 0; i < pointsCnt; i++) {
-        points[i] = points[i] * (0.42 - 0.5 * cos(2 * M_PI * i / pointsCnt) +
-                                 0.08 * cos(4 * M_PI * i / pointsCnt));
+        points[i] = points[i] * (0.42 - 0.5 * cos(2 * M_PI * i / (pointsCnt - 1)) +
+                                 0.08 * cos(4 * M_PI * i / (pointsCnt - 1)));
     }
 }
 
