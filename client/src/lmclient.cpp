f495bc842e7b982af6c07cb13f2405b0e8a3df02
Volodymyr Kononenko <vmkononenko@gmail.com>
2019-05-19 13:42:58 +0300

    [dev] Add licensing information
    Change-Id: I31870d93df0ec516563c75b63189d1402cbea59f

--- client/src/lmclient.cpp
+++ client/src/lmclient.cpp
@@ -1,10 +1,25 @@
 /*
- * lmclient.cpp
+ * Copyright 2019 Volodymyr Kononenko
  *
- * libmusic-dsp-client
+ * This file is part of Music-DSP.
  *
- * Command line interface for libmusic-dsp-client
+ * Music-DSP is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * Music-DSP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Music-DSP. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file    lmclient.cpp
+ * @brief   Command line interface for libmusic-dsp-client
  */
 
 #include <algorithm>
--- libmusic/src/music_scale.cpp
+++ libmusic/src/music_scale.cpp
@@ -1,7 +1,25 @@
-/**
- * music_scale.cpp
+/*
+ * Copyright 2019 Volodymyr Kononenko
  *
- * Implementation of the MusicScale class methods
+ * This file is part of Music-DSP.
+ *
+ * Music-DSP is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Music-DSP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Music-DSP. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file    music_scale.cpp
+ * @brief   Implementation of the MusicScale class methods
  */
 
 #include <algorithm>
