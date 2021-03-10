4bcd296145b5877f096d143e270c2becad06c86d
Misipuk <maxtretyakov37@gmail.com>
2021-03-10 20:38:06 +0200

    Bug fix

--- tools/DatasetExtraction/chord.py
+++ tools/DatasetExtraction/chord.py
@@ -7,21 +7,6 @@ This implementation tries to follow the references and their implementation
 (e.g., https://github.com/jayg996/BTC-ISMIR19)
 """
 
-import numpy as np
-import pandas as pd
-
-CHORD_DTYPE = [('root', np.int),
-               ('bass', np.int),
-               ('intervals', np.int, (12,)),
-               ('is_major', np.bool)]
-
-CHORD_ANN_DTYPE = [('start', np.float),
-                   ('end', np.float),
-                   ('chord', CHORD_DTYPE)]
-
-NO_CHORD = (-1, -1, np.zeros(12, dtype=np.int), False)
-UNKNOWN_CHORD = (-1, -1, np.ones(12, dtype=np.int) * -1, False)
-
 
 class Chord:
     def __init__(self, start_time, end_time, label):
