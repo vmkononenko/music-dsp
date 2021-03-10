05efc66d114bc2da1024a2e7920c5f7deed93bbe
Misipuk <maxtretyakov37@gmail.com>
2021-03-10 20:34:24 +0200

    Parsing and Ini matrix

--- tools/DatasetExtraction/chord.py
+++ tools/DatasetExtraction/chord.py
@@ -1,10 +1,43 @@
-#Simple chord implementation
+"""
+Simple chord implementation
+
+Notes
+-----
+This implementation tries to follow the references and their implementation
+(e.g., https://github.com/jayg996/BTC-ISMIR19)
+"""
+
+import numpy as np
+import pandas as pd
+
+CHORD_DTYPE = [('root', np.int),
+               ('bass', np.int),
+               ('intervals', np.int, (12,)),
+               ('is_major', np.bool)]
+
+CHORD_ANN_DTYPE = [('start', np.float),
+                   ('end', np.float),
+                   ('chord', CHORD_DTYPE)]
+
+NO_CHORD = (-1, -1, np.zeros(12, dtype=np.int), False)
+UNKNOWN_CHORD = (-1, -1, np.ones(12, dtype=np.int) * -1, False)
+
 
 class Chord:
-    def __init__(self):
-        pass
+    def __init__(self, start_time, end_time, label):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.label = label
 
-    def __init__(self, start, end, chord):
-        self.start = start
-        self.end = end
-        self.chord = chord
+    def __repr__(self):
+        return "({}, {}, {})".format(self.start_time, self.end_time, self.label)
+
+
+class Song:
+    def __init__(self, chords_list):
+        self.chords_list = chords_list
+        try:
+            if chords_list[0].label == 'N':
+                chords_list.pop(0)
+        except:
+            print("Error occured")
--- tools/DatasetExtraction/dataset.py
+++ tools/DatasetExtraction/dataset.py
@@ -1,37 +1,85 @@
 from abc import ABCMeta, abstractmethod
-from accessify import private, protected
-import chord
+from accessify import protected
+from chord import Chord, Song
+import numpy as np
+from glob import glob
 
 class Dataset:
     #i - song, j - chord
     songlist = [[]]
 
+
     @protected
     def validate(self):
-        for song in self.songlist:
-            for ch in song:
-                if not isinstance(ch, chord):
-                    raise Exception("Incorrect chord type")
-                    #will also chech cq_max
+        pass
 
-
-    #returns songlist with Chord class instances
     @protected
-    @abstractmethod
     def load(self):
         pass
 
-    @private
     def get_hmm_matrix_i(self):
         pass
 
-    @private
     def get_hmm_matrix_t(self):
         pass
 
-    @private
     def get_hmm_matrix_e(self):
         pass
 
 
+class Iso_Dataset(Dataset):
+
+    # assume we have chord_id dictionary which maps Chords into ids
+    def get_hmm_matrix_i(self, chord_id, dir_path):
+        chord_id = {}
+        song_list = self.load(dir_path)
+        i_matrix = np.array(len(chord_id), dtype=float)
+        for song in song_list:
+            i_matrix[chord_id.get(song.chords_list[0].label)] += 1 #для каждого аккорда получили кол-во раз, когда аккорд являлся первым в песне
+        i_matrix = i_matrix/len(song_list) #оцениваем вероятность через частоту
+        return i_matrix
+
+
+    #returns list of Songs class instances (Song class instance contains list of Chord class instances)
+    def load(self, dir_path):
+        song_list = []
+        file_list = glob(dir_path)
+        for file in file_list:
+            song_list.append(Song(self.load_chords(file)))
+        return song_list
+
+    #Load chords from a text file, returns list of Chord instances
+    def load_chords(self, filename):
+        chords_list = []
+        with open(filename, 'r') as f:
+            for line in f:
+                if line:
+                    splits = line.split()
+                    if len(splits) == 3:
+                        s = splits[0]
+                        e = splits[1]
+                        l = self.label_error_modify(splits[2])
+                        chords_list.append(Chord(float(s), float(e), l))
+        return chords_list
+
+    #removes label missprints
+    def label_error_modify(self, label):
+        if label == 'Emin/4':
+            label = 'E:min/4'
+        elif label == 'A7/3':
+            label = 'A:7/3'
+        elif label == 'Bb7/3':
+            label = 'Bb:7/3'
+        elif label == 'Bb7/5':
+            label = 'Bb:7/5'
+        elif label.find(':') == -1:
+            if label.find('min') != -1:
+                label = label[:label.find('min')] + ':' + label[label.find('min'):]
+        return label
+
+
+
+
+if __name__ == '__main__':
+    Iso_Dataset().load("D:\\ChordsRecognition\\music-dsp\\tools\\DatasetExtraction\\test_chords_txt\\*.txt")
 
