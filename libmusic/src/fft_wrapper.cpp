vmkononenko/music-dsp

commit c3cee21d2c7d40537d5693725cce675560a600fe

[GH #2] Fix FFTWrapper::FFTPruned()
Kudos to Oleksandr Natalenko for the bug report.

Change-Id: I875c448934c846441b42dff4ed7e0829d1586bc8

libmusic/src/fft_wrapper.cpp
@@ -76,10 +76,11 @@ fd_t FFTWrapper::FFTPruned(FFT *fft)
         fft_bin = fft->FreqToIdx(f, round);
         fd.push_back(fft->GetFreqDomain().p[fft_bin]);
         i++;
-        f =  pc_.getFreqByInterval(f, 1.0 / bpo_);
-    }
-    if (i % (bpo_ / notes_Total)) {
-        f = pc_.getPitch(f);
+        if (i % (bpo_ / notes_Total)) {
+            f = pc_.getPitch(f);
+        } else {
+            f =  pc_.getFreqByInterval(f, 1.0 / bpo_);
+        }
     }
 
     return fd;

commit c4446992bcee8a5c091536dfd97913c0052891a5

libmusic: fix deadloop in FFTWrapper::FFTPruned()
Regression introduced by commit:
   c3cee21d [GH #2] Fix FFTWrapper::FFTPruned()

It seems interval step and pitch calculation on semitone boundary are swapped for some reason.

libmusic/src/fft_wrapper.cpp
@@ -77,9 +77,9 @@ fd_t FFTWrapper::FFTPruned(FFT *fft)
         fd.push_back(fft->GetFreqDomain().p[fft_bin]);
         i++;
         if (i % (bpo_ / notes_Total)) {
-            f = pc_.getPitch(f);
-        } else {
             f =  pc_.getFreqByInterval(f, 1.0 / bpo_);
+        } else {
+            f = pc_.getPitch(f);
         }
     }
 